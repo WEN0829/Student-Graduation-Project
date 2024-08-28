7f23641c0d92c7ab2ae07b6be1412dc25b39cbb4
ian102991 <100286073+ian102991@users.noreply.github.com>
2024-08-28 16:03:16 +0800

    Update python_draw_map.py

--- python_draw_map.py
+++ python_draw_map.py
@@ -9,11 +9,11 @@ output = Output()
 map_taiwan = folium.Map(location=[23.6978, 120.9605], zoom_start=8, zoom_control=False, min_zoom=8, max_zoom=8)
 
 scenic_spots = {
-    '台北101': [25.033964, 121.564468],
-    '中正紀念堂': [25.035386, 121.516994],
-    '阿里山': [23.508245, 120.802216],
-    '日月潭': [23.854069, 120.917998],
-    '墾丁國家公園': [21.949725, 120.780817]
+    '士林夜市': [25.05, 121.31],
+    '九份老街': [25.111661, 121.804139],
+    '大稻埕馬頭': [25.03, 121.30],
+    '大溪老街': [25.026326, 121.738516],
+    '墾丁國家公園': [24.933441, 121.369747]
 }
 
 for spot, coords in scenic_spots.items():
@@ -34,27 +34,3 @@ for spot, coords in scenic_spots.items():
 map_taiwan.add_child(folium.ClickForMarker(popup=None).add_to(map_taiwan))
 map_taiwan.save("map_taiwan.html")
 display(output)
-'''
-import folium
-import webbrowser
-
-
-class Map:
-    def __init__(self, center, zoom_start):
-        self.center = center
-        self.zoom_start = zoom_start
-    
-    def showMap(self):
-        #Create the map
-        my_map = folium.Map(location = self.center, zoom_start = self.zoom_start)
-
-        #Display the map
-        my_map.save("map.html")
-        webbrowser.open("map.html")
-
-
-#Define coordinates of where we want to center our map
-coords = [51.5074, 0.1278]
-map = Map(center = coords, zoom_start = 13)
-map.showMap()
-'''
