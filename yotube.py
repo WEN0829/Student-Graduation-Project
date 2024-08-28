0ff37c7222b2f7ab9f299b1d136a289908071bc9
ian102991 <100286073+ian102991@users.noreply.github.com>
2024-08-28 17:59:42 +0800

    Update yotube.py

--- yotube.py
+++ yotube.py
@@ -19,6 +19,8 @@ currentDateAndTime = datetime.now()
 sys.path.append('CSRNet-pytorch')
 from model import CSRNet
 import json
+import random
+random.seed(a=None, version=2)
 def blur(frame,kernel_size):
     b,g,r=cv2.split(frame)
     b=tf.constant(b,dtype=tf.float32)
@@ -54,7 +56,9 @@ model1.load_state_dict(checkpoint)
 model = YOLO("yolov8m.pt")
 
 # YouTube 直播影片的網址
-url = "https://www.youtube.com/watch?v=fjhg3gAnMFg&list=PLlBCNJxYNaFzdUPNDmPbe4uXftZ8octo2&index=65"
+name=['士林夜市','大稻埕','九份','大溪老街','三峽老街','多良車站','高美濕地']
+choice=name[random.choice([0,1,2,3,4,5,6])]
+url = data[choice]['url']
 # 取得直播影片的串流
 streams = streamlink.streams(url)
 # 選擇最佳品質的串流
@@ -62,12 +66,13 @@ stream_url = streams["best"].url
 # 開啟影片串流
 cap = cv2.VideoCapture(stream_url)
 ret, frame = cap.read()
-print(frame.shape)
 frame1=frame.copy()
 if not ret:
     print("Cannot receive frame")
     exit()
-if currentDateAndTime.hour<=18:#士林夜市#改成yolo如果判斷人數小於某個數
+for i in data[choice]['mask']:
+    frame[i[0]:i[1],i[2]:i[3],:]=0
+if currentDateAndTime.hour<=18:#改成yolo如果判斷人數小於某個數
     #frame=blur(frame,15)
     pass
 img=Image.fromarray(frame)
