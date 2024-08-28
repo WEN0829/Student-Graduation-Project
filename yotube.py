7ddebb1e077e6e895561e4328c01e96ef37739da
ian102991 <100286073+ian102991@users.noreply.github.com>
2024-08-28 15:12:34 +0800

    加入mask(還沒加天氣辨識)

--- yotube.py
+++ yotube.py
@@ -13,8 +13,33 @@ import torchvision.transforms.functional as F
 import torch
 from torchvision import transforms
 from collections import OrderedDict
+from datetime import datetime
+import tensorflow as tf
+currentDateAndTime = datetime.now()
 sys.path.append('CSRNet-pytorch')
 from model import CSRNet
+def blur(frame,kernel_size):
+    b,g,r=cv2.split(frame)
+    b=tf.constant(b,dtype=tf.float32)
+    g=tf.constant(g,dtype=tf.float32)
+    r=tf.constant(r,dtype=tf.float32)
+    b=tf.expand_dims(b,axis=2)
+    g=tf.expand_dims(g,axis=2)
+    r=tf.expand_dims(r,axis=2)
+    filter=np.array(np.ones((kernel_size, kernel_size)),dtype=np.float32)*(1/kernel_size**2)
+    filter=tf.convert_to_tensor(filter,dtype=tf.float32)
+    filter=tf.reshape(filter,[kernel_size,kernel_size,1,1])
+    b=tf.nn.conv2d(tf.expand_dims(b,axis=0),filter,strides=[1,1,1,1],padding='SAME')
+    b=b.numpy()
+    b=b.astype(np.uint8)
+    g=tf.nn.conv2d(tf.expand_dims(g,axis=0),filter,strides=[1,1,1,1],padding='SAME')
+    g=g.numpy()
+    g=g.astype(np.uint8)
+    r=tf.nn.conv2d(tf.expand_dims(r,axis=0),filter,strides=[1,1,1,1],padding='SAME')
+    r=r.numpy()
+    r=r.astype(np.uint8)
+    frame=cv2.merge([b[0,:,:,0],g[0,:,:,0],r[0,:,:,0]])
+    return frame
 transform=transforms.Compose([
                        transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225]),
@@ -27,7 +52,7 @@ model1.load_state_dict(checkpoint)
 model = YOLO("yolov8m.pt")
 
 # YouTube 直播影片的網址
-url = "https://www.youtube.com/watch?v=2lVMTs_Fj0M&list=PLtPJtE4_gezcIXkVoH0pzSqqjN6FSjMHF&index=60"
+url = "https://www.youtube.com/watch?v=1lzlw6f3zZ8&list=PLDm4hXBol5DE9QmiOdgyzYmS2jRzenhUv&index=263"
 # 取得直播影片的串流
 streams = streamlink.streams(url)
 # 選擇最佳品質的串流
@@ -39,6 +64,12 @@ frame1=frame.copy()
 if not ret:
     print("Cannot receive frame")
     exit()
+if currentDateAndTime.hour<=18:#士林夜市
+    frame=blur(frame,25)
+    pass
+frame[0:120,0:800,:]=0#士林夜市
+#frame[0:147,1252:,:]=0#大稻埕
+#frame[0:74,650:1252,:]=0#大稻埕
 img=Image.fromarray(frame)
 img = transform(img.convert('RGB'))
 output = model1(img.unsqueeze(0))
